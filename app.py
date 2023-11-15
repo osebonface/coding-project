90fc6c4438cc05b2e6f5e3882c4eb964d6961812
osebonface <142354631+osebonface@users.noreply.github.com>
2023-11-15 05:06:34 +0300

    jh

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import kivy
 from kivy.app import App
 
+from kivy.uix.label import Label
+from kivy.clock import Clock
+
+
 from kivy.uix.boxlayout import BoxLayout
 
 class RegistrationApp(App):
@@ -28,4 +32,39 @@ class RegistrationApp(App):
         return layout
     if __name__ == '__main__':
         RegistrationApp().run()
-        
+
+class RegistrationApp(App):
+    def build(self):
+        self.title = "Registration Form"
+        layout = BoxLayout()
+        # Create a label for the title of the form
+        title_label = Label(text="Registration Form", font_size=20)
+        layout.add_widget(title_label)
+        # ... other input fields ...
+
+        # Create a submit button to register user
+        submit_button = Button(text='Register User', background_color=[1, 1, 1, .5], on_press=self.submit_form)
+        layout.add_widget(submit_button)
+        return layout
+
+    def submit_form(self, instance):
+        # Get input field values
+        fname = fname_input.text
+        lname = lname_input.text
+        email = email_input.text
+        pnum = pnum_input.text
+
+        # Check if inputs are not empty
+        if fname and lname and email and pnum:
+            # Change the layout to a success message
+            Clock.schedule_once(self.success_message, 1)
+
+    def success_message(self, dt):
+        # Remove the previous layout
+        self.root.clear_widgets()
+
+        # Create a success message
+        success_label = Label(text="Registration Successful!", font_size=20)
+        self.root.add_widget(success_label)
+
+
