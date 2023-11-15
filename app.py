dc4c5e8137894bc77652da618fa47ca9daea5ef1
osebonface <142354631+osebonface@users.noreply.github.com>
2023-11-15 04:57:15 +0300

    committed

--- app.py
+++ app.py
@@ -6,3 +6,25 @@ from kivy.uix.boxlayout import BoxLayout
 class RegistrationApp(App):
     def build(self):
         self.title = "Registration Form"
+        layout = BoxLayout()
+        # Create a label for the title of the form
+        title_label = Label(text="Registration Form", font_size=20)
+        layout.add_widget(title_label)
+        # Create an input field for first name
+        fname_input = TextInput(hint_text='First Name')
+        layout.add_widget(fname_input)
+        # Create an input field for last name
+        lname_input = TextInput(hint_text='Last Name')
+        layout.add_widget(lname_input)
+        # Create an input field for email address
+        email_input = TextInput(hint_text='Email Address', multiline=False, password=True)
+        layout.add_widget(email_input)
+        # Create an input field for phone number
+        pnum_input = TextInput(hint_text='Phone Number', multiline=False, password=True)
+        layout.add_widget(pnum_input)
+        # Create a submit button to register user
+        submit_button = Button(text='Register User', background_color=[1, 1, 1, .5])
+        layout.add_widget(submit_button)
+        return layout
+    if __name__ == '__main__':
+        RegistrationApp().run()
