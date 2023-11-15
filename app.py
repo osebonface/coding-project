029ceadbafa2c26564d155f290693bd16623d151
osebonface <142354631+osebonface@users.noreply.github.com>
2023-11-15 04:49:07 +0300

    am building registration form

--- app.py
+++ app.py
@@ -6,6 +6,3 @@ from kivy.uix.boxlayout import BoxLayout
 class RegistrationApp(App):
     def build(self):
         self.title = "Registration Form"
-    layout = BoxLayout(orientation = "vertical" , padding = 30,spacing = 10)
-
-    return layout
